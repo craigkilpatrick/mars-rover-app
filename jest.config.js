e6a8a8fb96d9785cf565f0c151feec13afb70eac
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2025-03-08 13:00:09 -0600

    test: configure jest for ES modules
    - feat(test): add jest configuration for ES modules
    - feat(test): add mock files for styles and assets
    - fix: handle axios and konva in transformIgnorePatterns

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  transformIgnorePatterns: [
+    "node_modules/(?!(axios|react-konva|konva)/)"
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/src/__mocks__/fileMock.js"
+  },
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/index.js",
+    "!src/reportWebVitals.js"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
