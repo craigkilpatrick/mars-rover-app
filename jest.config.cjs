903d28870d2fc3c358c1d7eb8f3cf590773f7e52
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2025-03-08 13:16:00 -0600

    fix(test): consolidate jest configuration
    - fix: rename jest.config.js to jest.config.cjs for CommonJS
    - fix: remove duplicate jest config from package.json
    - feat(test): update test scripts to use explicit config file

---
+++ jest.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios|react-konva|konva)/)'
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/src/__mocks__/fileMock.js'
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/reportWebVitals.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
