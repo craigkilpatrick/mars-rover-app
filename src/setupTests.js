b4a4789dd314a9703fa4a55f772854c9d5da75bd
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2025-03-08 13:28:39 -0600

    test: enhance test environment setup
    - feat(test): add Konva canvas mock
    - feat(test): add matchMedia mock for browser APIs
    - chore: clean up test environment configuration

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,17 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock window.matchMedia for Konva
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {},
+    addEventListener: function() {},
+    removeEventListener: function() {},
+    dispatchEvent: function() {},
+  };
+};
+
+// Set up canvas mock for Konva
+HTMLCanvasElement.prototype.getContext = jest.fn();
